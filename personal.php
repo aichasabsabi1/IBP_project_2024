20b03fcb9a48296094684752343e010582d60867
aichasabsabi1 <almlk0505@hotmail.com>
2024-06-11 19:44:07 +0300

    Add files via upload

---
+++ personal.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>COFFE Library</title>
+    </head>
+    <?php
+    include 'css.css';
+    include 'list.php';
+    ?>
+    <style type="text/css">
+        button{
+            background-color :#000;
+            border : none;
+            color:white;
+            padding : 15px 32px;
+            text-align : center;
+            display : inline;
+            margin : 4px 2px;
+            font-size: 16px;
+            width: 100%;
+        }
+        </style>
+
+<body>
+    <br><br><br><br>
+    <center>
+        <div style="width:500px:">
+        <a herf="chang.php"><button>chang the password</button></a>
+        <a herf=""><button>add a new book</button></a>
+        <a herf=""><button>the users</button></a>
+        <a herf=""><button>editing the home page</button></a>
+    </dive>
+    </center>
+   
+    
+   
+</body>
+</html>
---
+++ us.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>COFFE Library</title>
+</head>
+   
+       
+<body>
+    <?php
+    include 'css.css';
+    include 'list.php';
+    ?>
+    
+    <center>
+<div id="d3" style=" background-color: #a29196; width: 100%; height: 500PX;"> 
+        <br><br>
+            <h2>contact us</h2>
+        <form style="width: 80%;">
+            <div>
+                <label class="f25">  name </label>
+                <input class="f75" type=""name="">
+                
+            </div>
+            <div>
+                <label class="f25"> e-mail</label>
+                <input class="f75" type=""name="">
+            </div>
+            <div>
+                <label class="f25">number</label>
+                <input class="f75" type=""name="">
+            </div>
+           
+            <div>
+                <input style="width: 50%;"  type="submit" value="send">
+            </div>
+            </form>
+        
+    </div>
+            </center>
+</body>
+</html>
